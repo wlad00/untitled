fcfbe344f22082799ef5b3590a81aac2ea330350
wlad00 <wladsliw@list.ru>
2017-07-11 14:36:53 +0300

    Initial commit

---
+++ src/controller.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Vsevolod on 19.05.17.
+ */
+public class controller {
+
+    public static void main(String ... args) {
+
+        //public static Double sqr (double arg) {
+        ////    return arg;
+       // }
+
+       // Double d = sqr(3);
+
+       System.out.println(12);
+
+    }
+
+}
